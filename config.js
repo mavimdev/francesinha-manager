d43054a6a5cc7328b503edf465cbee343a41b5de
avim <carlos.avim@vodafone.com>
2021-01-31 19:39:13 +0000

    update mongo database url

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
   database: process.env.MONGODB_URI 
-  || 'mongodb://heroku_kpr4csbt:s3of4niqi8p19u93fl0ao9htqt@ds163016.mlab.com:63016/heroku_kpr4csbt' 
+  || 'mongodb+srv://mavimdev:1BmkfPHjvL21z3xv@cluster0.dc0km.mongodb.net/francesinhasdb?retryWrites=true&w=majority' 
   || 'localhost/nef'
 };
